90bdea7a759df221379537ee13e74642132be587
Jean-Paul <jp86mitter@gmail.com>
2019-10-25 22:11:04 -0500

    fixed issues regarding the increase/decrease change

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,6 +12,7 @@ import csv
 count_month = 0
 months = []
 bank_amount = []
+p_change = []
 
 #path to collect data from csv file
 pybank_csv = os.path.join('.','PyBank','Resources','budget_data.csv')
@@ -30,25 +31,36 @@ with open (pybank_csv, 'r') as csvfile:
         count_month += 1
         months.append(row[0])
         bank_amount.append(float(row[1]))
-        
-    #min value
-    minBankValue = min(bank_amount)
-
-    #max value
-    maxBankValue = max(bank_amount)
-
-    #finding the min month and the max month
-    for i in range(len(bank_amount)):
-
-        if minBankValue == bank_amount[i]:
-            minMonth = months[i]
-        elif maxBankValue == bank_amount[i]:
-            maxMonth = months[i]
 
     #the average change last month - first month/ total months-first month
     first_v = bank_amount[0]
     last_v = bank_amount[-1]
     avg_value = (last_v - first_v)/(count_month-1)
+
+    # for loop to get the monthly change in profit
+    # by subtracting the months
+    # make sure to subtract the len of the bank amount so you
+    # don't iterate past your last index when subtracting
+    for k in range(len(bank_amount)-1):
+        p_change.append(bank_amount[k+1]-bank_amount[k])
+    
+    #min value
+    minBankValue = min(p_change)
+
+    #max value
+    maxBankValue = max(p_change)
+
+    # finding the min month and the max month
+    # since we subtract the first month with second month
+    # and so on within the for loop, the p_change month index
+    # is not insync with the month list. Adding one to the
+    # increment resolves this problem
+    for i in range(len(p_change)):
+
+        if minBankValue == p_change[i]:
+            minMonth = months[i + 1]
+        elif maxBankValue == p_change[i]:
+            maxMonth = months[i + 1]
 
 #format floats to currency by creating a function
 def cur(amount):
@@ -62,8 +74,8 @@ print("Financial Analysis\n--------------------------------------\n")
 print(f"Total Months: {count_month}")
 print(f"Total: {cur(sum(bank_amount))}")
 print(f"Average Change: {cur(avg_value)}")
-print(f"Greatest Increase in Profits: {minMonth} {cur(maxBankValue)}")
-print(f"Greatest Decrease in Profits: {maxMonth} {cur(minBankValue)}")
+print(f"Greatest Increase in Profits: {maxMonth} {cur(maxBankValue)}")
+print(f"Greatest Decrease in Profits: {minMonth} {cur(minBankValue)}")
 
 #export to a text file
 with open("Bank_data.txt", "a+") as f:
@@ -71,5 +83,5 @@ with open("Bank_data.txt", "a+") as f:
     f.write(f"Total Months: {count_month}\n")
     f.write(f"Total: {cur(sum(bank_amount))}\n")
     f.write(f"Average Change: {cur(avg_value)}\n")
-    f.write(f"Greatest Increase in Profits: {minMonth} {cur(maxBankValue)}\n")
-    f.write(f"Greatest Decrease in Profits: {maxMonth} {cur(minBankValue)}\n")
+    f.write(f"Greatest Increase in Profits: {maxMonth} {cur(maxBankValue)}\n")
+    f.write(f"Greatest Decrease in Profits: {minMonth} {cur(minBankValue)}\n")
